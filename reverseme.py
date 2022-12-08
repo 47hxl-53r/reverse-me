42cbc513090adada56b705eeffa37ec597ae8701
47hx1-53r <99020554+47hxl-53r@users.noreply.github.com>
2022-12-09 01:43:20 +0530

    Initial commit

---
+++ reverseme.py
@@ -0,0 +1,43 @@
+import base64
+o_0_0_0_0='NfZnIwbV9iNDUzNjR'
+o0_0_0_0_0='fMTVfMzQ1eX0K'
+o_00_0_0_0='ZmxhZ3tkM2MwZD'
+o0o0o0o0o0='dKsTq2BZbNaqPlf2V7'
+ooo0ooo0oo='VGhpcyBpcyBub3QgYSBmbGFnCg=='
+o0000oo0oo='Zm9vbGVkCg=='
+ooooooooo0='bm90IHRoZSBmbGFnIGJybwo='
+oooooooooo='WW91cmVub3RjbG9zZWVub3VnaAo='
+o0oooooooo='bm93YXlicm95b3VyZWZvb2xkZWQK'
+o0000ooooo='ZG9udHRyeWRlY29kaW5naXMK'
+ox070100100='Q29uZ3JhdHMhIFlvdSBmb'
+ox01000000001='3VuZCB0aGUgZmlyc3Qga2V5Lgo='
+ox0x00o0ooo0o='SW52YWxp'
+ox00xoox00xo0='ZCBrZXkK'
+x07777777777=2235
+ox0c00x0c0000=round(x07777777777*45/5)
+oxcv000000000=1+1
+oxfgoo000000=o_00_0_0_0+o_0_0_0_0+o0_0_0_0_0
+i00=10+10+10+10+10+10+10+10+10+10
+em1n3m1sgr347=i00/5
+m0r7y_1s_01010101010=round(i00*5/i00)
+oud_7777777777=int(input("Key 1 : "))
+if oud_7777777777==round(ox0c00x0c0000/m0r7y_1s_01010101010):
+	n00bm4573r69ishere000=ox070100100+ox01000000001
+	g00fyb4ll5=base64.b64decode(n00bm4573r69ishere000).decode('utf-8')
+	print(g00fyb4ll5)
+	x077777777771=80062
+	tuentytwo=5+6*oxcv000000000/oxcv000000000
+	tuentyto=tuentytwo-9
+	r1ck_54nch3z=tuentyto*oxcv000000000/oxcv000000000
+	x07777777777_for_oxfgoo000000=int(input("Key 2 : "))
+	if x07777777777_for_oxfgoo000000==round(x077777777771/r1ck_54nch3z):
+		oxfgoo000000g=base64.b64decode(oxfgoo000000).decode('utf-8')
+		print(oxfgoo000000g)
+	else:
+		invalidx07777777777_encoded=ox0x00o0ooo0o+ox00xoox00xo0
+		invalidx07777777777=base64.b64decode(invalidx07777777777_encoded).decode('utf-8')
+		print(invalidx07777777777)
+else:
+	invalidx07777777777_encodedd=ox0x00o0ooo0o+ox00xoox00xo0
+	invalidx07777777777y=base64.b64decode(invalidx07777777777_encodedd).decode('utf-8')
+	print(invalidx07777777777y)
